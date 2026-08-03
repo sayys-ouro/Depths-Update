f474e310d297f1e8b41f3106023fa594bffc2c69
sayys <simonnnk16@gmail.com>
2026-08-03 18:00:26 +0200

    version bump

--- src/main/java/sayys/depthsupdate/Reference.java
+++ src/main/java/sayys/depthsupdate/Reference.java
@@ -5,5 +5,5 @@ public class Reference {
 
     public static final String MOD_ID = "depthsupdate";
     public static final String MOD_NAME = "Depths Update";
-    public static final String VERSION = "1.12.2-1.0.0-a11";
+    public static final String VERSION = "1.12.2-1.0.0-a12";
 }
